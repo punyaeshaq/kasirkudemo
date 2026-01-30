72e41dc9f4ddb1c8cfc1a8e8b3346b7899ee70eb
punyaeshaq <punyaeshaq@gmail.com>
2026-01-31 01:12:10 +0800

    Patch env loading for Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,15 @@
 <?php
 
+// VERCEL PATCH: Ensure environment variables are loaded
+if (isset($_SERVER['APP_KEY'])) {
+    putenv('APP_KEY=' . $_SERVER['APP_KEY']);
+    $_ENV['APP_KEY'] = $_SERVER['APP_KEY'];
+}
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+*/
+
 require __DIR__ . '/../public/index.php';
