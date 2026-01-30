ff5e67f585f25083f77f136d0629409722453259
punyaeshaq <punyaeshaq@gmail.com>
2026-01-31 01:17:25 +0800

    Comprehensive env patch for Vercel

--- api/index.php
+++ api/index.php
@@ -1,9 +1,40 @@
 <?php
 
-// VERCEL PATCH: Ensure environment variables are loaded
-if (isset($_SERVER['APP_KEY'])) {
-    putenv('APP_KEY=' . $_SERVER['APP_KEY']);
-    $_ENV['APP_KEY'] = $_SERVER['APP_KEY'];
+/*
+|--------------------------------------------------------------------------
+| VERCEL PATCH: Inject Environment Variables
+|--------------------------------------------------------------------------
+|
+| Vercel passes environment variables through $_SERVER. Laravel's env()
+| helper relies on getenv() and $_ENV. This patch syncs them.
+|
+*/
+
+// List of critical environment variables that must be injected
+$envVars = [
+    'APP_NAME',
+    'APP_ENV',
+    'APP_KEY',
+    'APP_DEBUG',
+    'APP_URL',
+    'LOG_CHANNEL',
+    'DB_CONNECTION',
+    'DB_HOST',
+    'DB_PORT',
+    'DB_DATABASE',
+    'DB_USERNAME',
+    'DB_PASSWORD',
+    'CACHE_DRIVER',
+    'SESSION_DRIVER',
+    'QUEUE_CONNECTION',
+    'FILESYSTEM_DISK',
+];
+
+foreach ($envVars as $var) {
+    if (isset($_SERVER[$var]) && !empty($_SERVER[$var])) {
+        putenv("$var=" . $_SERVER[$var]);
+        $_ENV[$var] = $_SERVER[$var];
+    }
 }
 
 /*
